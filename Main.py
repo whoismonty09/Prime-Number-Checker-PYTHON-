42451979ccb8b73e5e6cd50098822fc5867e952f
Mayank malviya <mayankmalviya440@gmail.com>
2026-01-31 08:11:13 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,25 @@
+print("Prime Number Checker developed by Monty")
+while True:
+    try:
+        num = int(input("Enter a number (or type 0 to exit):"))
 
+        if num == 0:
+            print('Good Byee!')
+            break
+
+        if num <=1:
+            print("Not a prime number")
+        else:
+            is_prime = True
+            for i in range (2 ,int(num ** 0.5) +1):
+                if num % i == 0:
+                    is_prime = False
+                    break
+
+            if is_prime:
+                print("Prime Number")
+            else:
+                print("Not a Prime Number")  
+
+    except ValueError:
+        print("Please enter a valid integer!")                        
